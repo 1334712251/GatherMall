4ffac9f29e875cedcc76c34e4f7dcb4dd8ac47da
linhao <1334712251@qq.com>
2021-09-02 21:20:00 +0800

    提交结果

--- renren-generator/src/main/java/io/renren/RenrenGeneratorApplication.java
+++ renren-generator/src/main/java/io/renren/RenrenGeneratorApplication.java
@@ -8,9 +8,9 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @MapperScan("io.renren.dao")
-public class RenrenApplication {
+public class RenrenGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RenrenApplication.class, args);
+		SpringApplication.run(RenrenGeneratorApplication.class, args);
 	}
 }
