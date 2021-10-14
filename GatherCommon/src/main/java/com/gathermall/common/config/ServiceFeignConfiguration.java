a58f2cd41552e3c0eb0881e69e07a810fe4c9ebe
linhao <1334712251@qq.com>
2021-10-14 17:06:38 +0800

    提交结果

---
+++ GatherCommon/src/main/java/com/gathermall/common/config/ServiceFeignConfiguration.java
@@ -0,0 +1,26 @@
+package com.gathermall.common.config;
+
+import feign.Request;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Feign配置
+ *
+ * @author Zachary 2019-09-19
+ */
+@Configuration
+public class ServiceFeignConfiguration {
+
+    @Value("${service.feign.connectTimeout:60000}")
+    private int connectTimeout;
+
+    @Value("${service.feign.readTimeOut:60000}")
+    private int readTimeout;
+
+    @Bean
+    public Request.Options options() {
+        return new Request.Options(connectTimeout, readTimeout);
+    }
+}
