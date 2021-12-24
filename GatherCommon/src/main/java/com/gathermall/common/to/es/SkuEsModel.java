95be618faa799cb4866a7537de0e6780da27acbd
linhao <1334712251@qq.com>
2021-12-24 19:59:14 +0800

    提交结果

--- GatherCommon/src/main/java/com/gathermall/common/to/es/SkuEsModel.java
+++ GatherCommon/src/main/java/com/gathermall/common/to/es/SkuEsModel.java
@@ -1,6 +1,7 @@
 package com.gathermall.common.to.es;
 
 
+import jdk.internal.util.xml.impl.Attrs;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,38 +15,85 @@ public class SkuEsModel {
 
     private Long spuId;
 
+    /**
+     * 商品标题
+     */
     private String skuTitle;
 
+    /**
+     * 商品价格
+     */
     private BigDecimal skuPrice;
 
+    /**
+     * 商品图片
+     */
     private String skuImg;
 
+    /**
+     * 销量
+     */
     private Long saleCount;
 
+    /**
+     * 是否有货
+     */
     private Boolean hasStock;
 
+    /**
+     * 热度评分
+     */
     private Long hotScore;
 
+    /**
+     * 品牌id
+     */
     private Long brandId;
 
-    private Long catalogId;
-
+    /**
+     * 品牌名字
+     */
     private String brandName;
 
+    /**
+     * 品牌图片
+     */
     private String brandImg;
 
+    /**
+     * 分类id
+     */
+    private Long catalogId;
+
+    /**
+     * 分类名字
+     */
     private String catalogName;
 
+    /**
+     * 商品属性（共性）
+     */
     private List<Attrs> attrs;
 
 
+    /**
+     * CPU ： i9，i8,i5
+     * 尺寸：1米，两米，三米
+     * 。。。。。。
+     */
     @Data
     public static class Attrs implements Serializable {
 
         private Long attrId;
 
+        /**
+         * 属性名字
+         */
         private String attrName;
 
+        /**
+         * 属性数值
+         */
         private String attrValue;
     }
 }
